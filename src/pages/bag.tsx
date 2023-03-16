69f660883b432472cf14889883056d5cb5b1886b
Thomas Esimple <thomas.cipriani@esimple.it>
2023-03-16 16:38:07 +0100

    Tabs

--- src/pages/bag.tsx
+++ src/pages/bag.tsx
@@ -2,12 +2,15 @@ import { type NextPage } from "next";
 import { useState } from "react";
 import player_counts from "~/data/player_counts";
 import Range from "../components/ui/Range";
+import { cx } from "class-variance-authority";
+import Tab from "~/components/ui/Tab";
 
 const BagPage: NextPage = () => {
   const [players, setPlayers] = useState(8);
-  const { townsfolk, outsider, minion, demon } = player_counts.has(players)
-    ? player_counts.get(players)!
-    : player_counts.get(5)!;
+  const [tab, setTab] = useState("T");
+  const { townsfolk, outsider, minion, demon } = player_counts[players]
+    ? player_counts[players]!
+    : player_counts[5]!;
 
   return (
     <>
@@ -20,11 +23,43 @@ const BagPage: NextPage = () => {
           value={players}
           onChange={setPlayers}
         />
-        <div className="flex">
-          <span>TS: {townsfolk}</span>
-          <span>O: {outsider}</span>
-          <span>M: {minion}</span>
-          <span>D: {demon}</span>
+
+        <div className="flex justify-around gap-2">
+          <span>{townsfolk}</span>
+          <span>{outsider}</span>
+          <span>{minion}</span>
+          <span>{demon}</span>
+        </div>
+
+        <div className="tabs justify-between">
+          <Tab
+            className="grow"
+            onClick={() => setTab("T")}
+            active={tab === "T"}
+          >
+            T
+          </Tab>
+          <Tab
+            className="grow"
+            onClick={() => setTab("O")}
+            active={tab === "O"}
+          >
+            O
+          </Tab>
+          <Tab
+            className="grow"
+            onClick={() => setTab("M")}
+            active={tab === "M"}
+          >
+            M
+          </Tab>
+          <Tab
+            className="grow"
+            onClick={() => setTab("D")}
+            active={tab === "D"}
+          >
+            D
+          </Tab>
         </div>
       </main>
     </>
